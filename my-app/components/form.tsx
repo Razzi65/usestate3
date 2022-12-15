4381f8d9a70aceb86c50afcdc258462ef570d281
Razzi65 <rizmoh65@gmail.com>
2022-12-15 15:44:00 +0500

    to do 11 done with firebase

---
+++ my-app/components/form.tsx
@@ -0,0 +1,14 @@
+import { FormControl, Flex, Input, FormLabel } from '@chakra-ui/react'
+import { Container, Box, Heading, Spacer, Button, ButtonGroup } from '@chakra-ui/react'    
+
+const Form = () => {
+
+  return (
+    <div>
+        </div>
+
+  )
+}
+
+
+export default Form
